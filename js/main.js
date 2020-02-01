5a78e364820fba9a8c69a9a88f94be445e57d831
Chester Guinto <cguinto@rapiddisplays.com>
2020-01-31 17:09:30 -0800

    Added basic navbar functionality! Styling to come soon.  Committer: Chester Guinto 	modified:   css/style.css 	modified:   index.html 	new file:   js/main.js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    var sidebar = document.getElementById("myNav");
+    sidebar.style.width = "10%";
+}
+
+function closeNav() {
+    var sidebar = document.getElementById("myNav");
+    sidebar.style.width = "0%";
+}
