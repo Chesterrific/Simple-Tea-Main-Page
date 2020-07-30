1044fa16f098e5602aec322408707f2ffd0aa861
Chester Guinto <chesterguinto@gmail.com>
2020-07-29 17:22:46 -0700

    initial commit for updated website layout.

---
+++ js/nav.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+  // Variables
+  var isWhite = true,
+    logo = document.getElementById('logo'),
+    welcomeEl = document.getElementById('welcome'),
+    missionEl = document.getElementById('mission'),
+    showcaseEl = document.getElementById('showcase');
+
+  $(window).bind('scroll', function () {
+    // Mission section's distance from top of window starting from its top.
+    var topOfMission = missionEl.offsetTop;
+
+    // Mission section's distance from the top of window startinf from it's bottom.
+    var botOfMission = missionEl.offsetTop + missionEl.offsetHeight;
+
+    // Logo's distance from the top of window + half its height.
+    var topOfLogo = window.scrollY + (logo.offsetTop + logo.offsetHeight / 2);
+
+    if (topOfLogo >= topOfMission && isWhite == true) {
+      $('#logo').css('color', 'black');
+      isWhite = false;
+    }
+    else if (!(topOfLogo >= topOfMission) && isWhite == false){
+      $('#logo').css('color', 'white');
+      isWhite = true;
+    }
+  });
+});
