2507d5eadcaec12c9a5f8340ab8df04c968c1933
Chester Guinto <chesterguinto@gmail.com>
2020-08-04 17:23:42 -0700

    Updated site with correct content images.

--- js/nav.js
+++ js/nav.js
@@ -3,14 +3,14 @@ $(document).ready(function () {
   // Variables
   var isWhite = true,
     logo = document.getElementById('logo'),
-    missionEl = document.getElementById('mission');
+    transitionEl = document.getElementById('transition');
 
   $(window).bind('scroll', function () {
     // Mission section's distance from top of window starting from its top.
-    var topOfMission = missionEl.offsetTop;
+    var topOfMission = transitionEl.offsetTop;
 
     // Mission section's distance from the top of window startinf from it's bottom.
-    var botOfMission = missionEl.offsetTop + missionEl.offsetHeight;
+    var botOfMission = transitionEl.offsetTop + transitionEl.offsetHeight;
 
     // Logo's distance from the top of window + half its height.
     var topOfLogo = window.scrollY + (logo.offsetTop + logo.offsetHeight / 2);
