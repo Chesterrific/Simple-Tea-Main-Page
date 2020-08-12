6318481d36c4cf0afb7251d29b6dcfa014930188
Chester Guinto <chesterguinto@gmail.com>
2020-08-11 18:09:08 -0700

    Added whiteout overlay. Changed logo and menu button transition location.

--- js/main.js
+++ js/main.js
@@ -3,26 +3,25 @@
 $(document).ready(function () {
   /* ----------------Navbar JS---------------- */
   // Scrolling changes for logo and menu
-  let logo = document.getElementById('#logo');
-  let menu = document.getElementById('#menu span');
-  let welcomeTop = $('#welcome').offset().top;
-  let missionTop = $('#mission').offset().top;
-  let scrollCounter = $(window).scrollTop() - welcomeTop;
+  let logo = $('#logo');
+  let menu = $('#menu span');
+  let missionTop = $('#transition').offset().top;
   let navOpen = false;
 
-  $(window).bind('scroll', function () {
-    welcomeTop = $('#welcome').offset().top;
-    missionTop = $('#mission').offset().top;
-    scrollCounter = $(window).scrollTop() - welcomeTop;
+  //Set overlay height to document height.
+  $('#overlay').css('height', $(document).height() + 'px',);
 
-    if (scrollCounter < missionTop) {
+  $(window).bind('scroll', function () {
+    logoMid = $('#logo').offset().top + $('#logo').height() / 2;
+
+    if (logoMid < missionTop) {
       if (!navOpen) {
-        $('#menu span').css('background', 'white');
-        $('#logo').css('color', 'white');
+        menu.css('background', 'white');
+        logo.css('color', 'white');
       }
     } else {
-      $('#menu span').css('background', 'black');
-      $('#logo').css('color', 'black');
+      menu.css('background', 'black');
+      logo.css('color', 'black');
     }
   });
 
@@ -31,16 +30,23 @@ $(document).ready(function () {
     $(this).toggleClass('open');
     $('#menu-nav').toggleClass('open');
 
+    // Toggle navOpen, first click makes it true.
     navOpen = !navOpen;
 
     if (navOpen) {
       $('#menu span').css('background', 'black');
       $('#logo').css('color', 'black');
-      $('#overlay').css('opacity', '0.8');
-    } else if (!navOpen && scrollCounter < missionTop) {
-      $('#menu span').css('background', 'white');
-      $('#logo').css('color', 'white');
-      $('#overlay').css('opacity', '0');
+      $('#overlay').toggleClass('open');
+    } else {
+      $('#overlay').toggleClass('open');
+
+      if (logoMid < missionTop) {
+        $('#menu span').css('background', 'white');
+        $('#logo').css('color', 'white');
+      } else {
+        $('#menu span').css('background', 'black');
+        $('#logo').css('color', 'black');
+      }
     }
   });
 
